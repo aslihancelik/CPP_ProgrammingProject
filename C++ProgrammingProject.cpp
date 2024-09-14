8812021a134686c54d81a81485d8c4899c8c5854
aslihancelik <aslihancelik27@gmail.com>
2024-09-13 23:23:59 -0400

    Changed the name of the constant.

--- C++ProgrammingProject.cpp
+++ C++ProgrammingProject.cpp
@@ -20,17 +20,19 @@ double distance( const double gasTank, const double averageMilesPerGallon)
 int main()
 {
     // Constants for the car's gas tank size and fuel efficiency (miles per gallon) in town and on the highway
-    const double tankSize = 20.0, TOWN_MILES_PER_GALLON = 23.5, HIGHWAY_MILES_PER_GALLON = 28.9;
+    const double TANK_SIZE = 20.0, TOWN_MILES_PER_GALLON = 23.5, HIGHWAY_MILES_PER_GALLON = 28.9;
 
     double town_distance, highway_distance;
 
     //Calculating the distance that car can travel on one full tank in town
-    town_distance = distance(tankSize, TOWN_MILES_PER_GALLON);
+    town_distance = distance(TANK_SIZE, TOWN_MILES_PER_GALLON);
 
     //Calculating the distance that car can travel on one full tank on highway
-    highway_distance = distance(tankSize, HIGHWAY_MILES_PER_GALLON);
+    highway_distance = distance(TANK_SIZE, HIGHWAY_MILES_PER_GALLON);
 
-    cout << "The distance that a car with a " << tankSize << " gallon gas tank can travel in town is " << town_distance << " miles. \n";
-    cout << "The distance that a car with a " << tankSize << " gallon gas tank can travel on the highway is " << highway_distance << " miles. \n";
+    cout << "The distance that a car with a " << TANK_SIZE << " gallon gas tank can travel in town is " << town_distance << " miles. \n";
+    cout << "The distance that a car with a " << TANK_SIZE << " gallon gas tank can travel on the highway is " << highway_distance << " miles. \n";
+
+    return 0;
 }
 
